edc04043a7ee8ca16185a688f1ae3caaabe1802f
Maryna Sapiton <m.sapiton@godeltech.com>
2018-10-27 16:04:44 +0300

    first commit

---
+++ src/MainClass.java
@@ -0,0 +1,6 @@
+public class MainClass {
+    public static void main (String args[]){
+Zadacha15 z15 = new Zadacha15();
+z15.solving();
+    }
+}
---
+++ src/Zadacha15.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Zadacha15 {
+
+String alphabet="абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+
+String message;
+
+    public void solving(){
+        char[] chars=alphabet.toCharArray();
+        char[] mes=message.toCharArray();
+
+        Scanner reader = new Scanner(System.in);
+
+        System.out.println("Введите сообщение");
+        message = reader.next();
+//если символ из алфавита совпадает с символом из сообщения выводим индекс символа из алфавита +1
+        for(int j=0;j<mes.length;j++){
+            for (int i=0;i<alphabet.length();i++){
+
+               if(mes[j]==chars[i]) {
+                   System.out.print(i + 1);
+               }
+            }
+        }
+
+
+
+    }
+}
